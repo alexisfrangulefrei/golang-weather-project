f69745a7c6a53c888d0ff431959be0dea46da337
Alexis FRANGUL <alexis.frangul@efrei.net>
2026-05-28 00:38:19 +0200

    partie E

---
+++ query.go
@@ -0,0 +1,53 @@
+package main
+
+func FilterByCountry(stations []Station, ISOCode string) []Station {
+	var filteredStationsByCountry []Station
+
+	for _, station := range stations {
+		if station.Country == ISOCode {
+			filteredStationsByCountry = append(filteredStationsByCountry, station)
+		}
+	}
+
+	return filteredStationsByCountry
+}
+
+func AvgTemperature(station Station) float64 {
+	if len(station.Observations) == 0 {
+		return 0
+	}
+
+	var total float64
+
+	for _, observation := range station.Observations {
+		total += observation.Temperature
+	}
+
+	return total / float64(len(station.Observations))
+}
+
+func MaxWindGust(stations []Station) (Station, float64) {
+	var maxWindGustStation Station
+	var maxWindGust float64
+
+	for _, station := range stations {
+		for _, observation := range station.Observations {
+			if observation.Wind.Speed > maxWindGust {
+				maxWindGustStation = station
+				maxWindGust = observation.Wind.Speed
+			}
+		}
+	}
+
+	return maxWindGustStation, maxWindGust
+}
+
+func CountByCountry(stations []Station) map[string]int {
+	stationsCountByCountry := make(map[string]int)
+
+	for _, station := range stations {
+		stationsCountByCountry[station.Country]++
+	}
+
+	return stationsCountByCountry
+}
